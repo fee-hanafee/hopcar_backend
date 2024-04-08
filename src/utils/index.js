b975e369e386ad4387829c4c099604879026fc4d
fee-hanafee <hanafee.pass@gmail.com>
2024-04-08 18:52:33 +0700

    setup project

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+const utils = {
+  jwt: require("./jwt"),
+  bcrypt: require("./bcrypt"),
+  catchError: require("./catch-error"),
+  cloundinaryUpload: require("./cloundinaryUpload"),
+  checkExistCredential: require("./checkExistCredential"),
+};
+
+module.exports = utils;
